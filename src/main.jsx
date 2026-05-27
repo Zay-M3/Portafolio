62af4678693fe0f9007a00bdb88c96f57e035133
Zay-M3 <oscardavidestradabetancourth@gmail.com>
2026-05-27 17:25:45 +0000

    feat: configurar react-router v7

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,7 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import { router } from '@router/index'
 
-import LandingPage from "./pages/LandingPage.jsx";
-import NoFound from "./pages/no-found/NoFound.jsx";
-import Layout from "./layout/Layout.jsx";
-import Galaxy from "./pages/Galaxy.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Layout>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/galaxias" element={<Galaxy />} />
-        <Route path="*" element={<NoFound />} />
-      </Routes>
-    </Layout>
-  </BrowserRouter>
-);
+createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
