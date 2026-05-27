eef9ec192eaf7bd9ccc6a2097dfdc8705a8d6fe3
Oscar Estrada Betancout <oscardavidestradabetancourth@gmail.com>
2026-05-27 12:25:58 -0500

    feat: configurar react-router v7 (#5)

---
+++ src/pages/index.ts
@@ -0,0 +1,7 @@
+// Page exports - will be renamed in block 7
+export { default as HomePage } from './Home.jsx'
+export { default as ProjectsPage } from './Proyects.jsx'
+export { default as AboutPage } from './AboutMe.jsx'
+export { default as ContactPage } from './ContacMe.jsx'
+export { default as GalaxyPage } from './Galaxy.jsx'
+export { default as NotFoundPage } from './NotFound.jsx'
